9746d38940f9851634e777f087499306d0ca99de
wuyouniyanhu <1763283557@qq.com>
2020-08-31 22:34:42 +0800

    ip_addr -> ip4_addr

---
+++ PR_Lwip211/pr_app/Debug/src/main.d
@@ -0,0 +1,162 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../Lwip211_bsp/microblaze_0/include/xparameters.h \
+ C:\Users\HQ_home\Desktop\ICAP\PR_Lwip211\PR_Lwip211\pr_app\src\pr_tftp_lib/pr_tftp.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/tcp.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/opt.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwipopts.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/debug.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/arch.h \
+ ../../Lwip211_bsp/microblaze_0/include/arch/cc.h \
+ ../../Lwip211_bsp/microblaze_0/include/arch/cpu.h \
+ ../../Lwip211_bsp/microblaze_0/include/mb_interface.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_types.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_assert.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_exception.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/tcpbase.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/mem.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/pbuf.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/err.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/ip.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/def.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/ip_addr.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/ip4_addr.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/ip6_addr.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/def.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/netif.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/stats.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/memp.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/priv/memp_std.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/priv/memp_priv.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/priv/mem_priv.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/ip4.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/prot/ip4.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/ip6.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/prot/ip.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/icmp.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/prot/icmp.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/udp.h \
+ ../../Lwip211_bsp/microblaze_0/include/lwip/prot/udp.h \
+ ../../Lwip211_bsp/microblaze_0/include/xhwicap.h \
+ ../../Lwip211_bsp/microblaze_0/include/xhwicap_i.h \
+ ../../Lwip211_bsp/microblaze_0/include/xhwicap_l.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_types.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_assert.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_io.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_printf.h \
+ ../../Lwip211_bsp/microblaze_0/include/xparameters.h \
+ ../../Lwip211_bsp/microblaze_0/include/bspconfig.h \
+ ../../Lwip211_bsp/microblaze_0/include/mb_interface.h \
+ ../../Lwip211_bsp/microblaze_0/include/xstatus.h \
+ ../../Lwip211_bsp/microblaze_0/include/netif/xadapter.h \
+ ../../Lwip211_bsp/microblaze_0/include/netif/xtopology.h \
+ ../src/platform.h ../src/platform_config.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_printf.h \
+ ../../Lwip211_bsp/microblaze_0/include/xil_cache.h
+
+../../Lwip211_bsp/microblaze_0/include/xparameters.h:
+
+C:\Users\HQ_home\Desktop\ICAP\PR_Lwip211\PR_Lwip211\pr_app\src\pr_tftp_lib/pr_tftp.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/tcp.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/opt.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwipopts.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/debug.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/arch.h:
+
+../../Lwip211_bsp/microblaze_0/include/arch/cc.h:
+
+../../Lwip211_bsp/microblaze_0/include/arch/cpu.h:
+
+../../Lwip211_bsp/microblaze_0/include/mb_interface.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_types.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_assert.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_exception.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/tcpbase.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/mem.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/pbuf.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/err.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/ip.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/def.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/ip_addr.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/ip4_addr.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/ip6_addr.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/def.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/netif.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/stats.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/memp.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/priv/memp_std.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/priv/memp_priv.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/priv/mem_priv.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/ip4.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/prot/ip4.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/ip6.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/prot/ip.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/icmp.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/prot/icmp.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/udp.h:
+
+../../Lwip211_bsp/microblaze_0/include/lwip/prot/udp.h:
+
+../../Lwip211_bsp/microblaze_0/include/xhwicap.h:
+
+../../Lwip211_bsp/microblaze_0/include/xhwicap_i.h:
+
+../../Lwip211_bsp/microblaze_0/include/xhwicap_l.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_types.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_assert.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_io.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_printf.h:
+
+../../Lwip211_bsp/microblaze_0/include/xparameters.h:
+
+../../Lwip211_bsp/microblaze_0/include/bspconfig.h:
+
+../../Lwip211_bsp/microblaze_0/include/mb_interface.h:
+
+../../Lwip211_bsp/microblaze_0/include/xstatus.h:
+
+../../Lwip211_bsp/microblaze_0/include/netif/xadapter.h:
+
+../../Lwip211_bsp/microblaze_0/include/netif/xtopology.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_printf.h:
+
+../../Lwip211_bsp/microblaze_0/include/xil_cache.h:
--- PR_Lwip211/pr_app/Debug/src/pr_tftp_lib/subdir.mk
+++ PR_Lwip211/pr_app/Debug/src/pr_tftp_lib/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/pr_tftp_lib/%.o: ../src/pr_tftp_lib/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MicroBlaze gcc compiler'
-	mb-gcc -Wall -O0 -g3 -I../../Lwip211_bsp/microblaze_0/include -I"C:\Users\HQ_home\Desktop\PR_Lwip211\pr_app\src" -I"C:\Users\HQ_home\Desktop\PR_Lwip211\pr_app\src\pr_tftp_lib" -c -fmessage-length=0 -MT"$@" -mlittle-endian -mcpu=v11.0 -mxl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	mb-gcc -Wall -O0 -g3 -I../../Lwip211_bsp/microblaze_0/include -I"C:\Users\HQ_home\Desktop\ICAP\PR_Lwip211\PR_Lwip211\pr_app\src" -I"C:\Users\HQ_home\Desktop\ICAP\PR_Lwip211\PR_Lwip211\pr_app\src\pr_tftp_lib" -c -fmessage-length=0 -MT"$@" -mlittle-endian -mcpu=v11.0 -mxl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
